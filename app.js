86b5e42d93f3720b34dd035588c3abc60bf3d429
Neiva07 <lucasneiva07@gmail.com>
2018-11-21 16:00:56 -0300

    Stream working

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 const express = require('express');
 const app = express();
 const fs = require('file-system');
-const path = 'techlead.mp4';
 
-app.get('/', function(req,res) {
-    
+app.use(express.static('./'));
+
+app.get('/video', function(req, res) {
+  const path = 'techlead.mp4'
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] 
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+});
+
+app.get('/', function(req, res) {
+    res.render('index.html')
 })
 
 app.listen(3000, function () {
