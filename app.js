fc0f5dfda58ab4e26a92d7eecc330e58f89aa23d
Neiva07 <lucasneiva07@gmail.com>
2018-11-21 15:09:45 -0300

    server created and added a video

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const fs = require('file-system');
+const path = 'techlead.mp4';
+
+app.get('/', function(req,res) {
+    
+})
+
+app.listen(3000, function () {
+    console.log('Video Stream Server is on!!');
+})
